a550c05f14a7b7b7ac4c74c10a3cdde97c09c232
ArneMotmans <motmans.arne@gmail.com>
2017-05-13 11:07:18 +0200

    Adding script for adding all data to main page

---
+++ Front end/MonkeyBusinessProject/Src/Scripts/dataFunctions.js
@@ -0,0 +1,35 @@
+/**
+ * Created by 11502064 on 12/05/2017.
+ */
+
+function addDataToTable() {
+    fetch('http://192.168.47.134/~user/Monkey_Business/src/get.php', {
+        method: 'get',
+        dataType: 'json',
+    }).then(function (response) {
+        var data = JSON.stringify(response);
+        var row = document.createElement("TR");
+        var cel_1 = document.createElement("TD");
+        var cel_2 = document.createElement("TD");
+        var cel_3 = document.createElement("TD");
+        var cel_4 = document.createElement("TD");
+        var cel_5 = document.createElement("TD");
+
+        cel_1.append(response[0]);
+        cel_2.append(response[1]);
+        cel_3.append(response[2]);
+        cel_4.append(response[3]);
+        cel_5.append(response[4]);
+
+        row.appendChild(cel_1);
+        row.appendChild(cel_2);
+        row.appendChild(cel_3);
+        row.appendChild(cel_4);
+        row.appendChild(cel_5);
+
+        document.getElementById("dataTable").appendChild(row);
+
+        alert(response);
+        console.log('success', data);
+    })
+}
