084698e226b1178176a5f0c8b03d0be982cc961a
yentemartens <11502392@student.pxl.be>
2017-05-17 11:14:39 +0200

    voor en achternaam weg gedaan

--- Front end/MonkeyBusinessProject/Src/Scripts/script.js
+++ Front end/MonkeyBusinessProject/Src/Scripts/script.js
@@ -6,8 +6,6 @@ window.addEventListener('load', windowLoaded);
 function windowLoaded() {
     document.getElementById('submit').addEventListener("click", function () {
         $credentials = new Array();
-        $credentials['first_name'] = document.getElementById('voornaam').value;
-        $credentials['last_name'] = document.getElementById('achternaam').value;
         $credentials['event_name'] = document.getElementById('eventnaam').value;
         $credentials['start_date'] = document.getElementById('startdatum').value;
         $credentials['end_date'] = document.getElementById('einddatum').value;
@@ -20,15 +18,11 @@ function addToDatabase(value) {
     fetch('http://192.168.180.128/~user/Monkey_Business/events', {
         method: "POST",
         body: JSON.stringify({
-            first_name: value.first_name,
-            last_name: value.last_name,
             event_name: value.event_name,
             start_date: value.start_date,
             end_date: value.end_date
         })
     }).then(function () {
-        document.getElementById('voornaam').value = "";
-        document.getElementById('achternaam').value = "";
         document.getElementById('eventnaam').value = "";
         document.getElementById('startdatum').value = "";
         document.getElementById('einddatum').value = "";
